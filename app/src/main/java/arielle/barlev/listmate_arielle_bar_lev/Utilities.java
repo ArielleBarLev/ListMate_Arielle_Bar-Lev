645bfd152789ce98b5dee1581d7fbf6f9570351e
Arielle Bar-Lev <arielle7.barlev.com>
2025-03-20 12:06:18 +0200

    Fix make_snackbar function

--- app/src/main/java/arielle/barlev/listmate_arielle_bar_lev/Utilities.java
+++ app/src/main/java/arielle/barlev/listmate_arielle_bar_lev/Utilities.java
@@ -7,25 +7,55 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
-import com.google.android.material.snackbar.BaseTransientBottomBar;
 import com.google.android.material.snackbar.Snackbar;
+
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Utilities {
 
+    private Queue<String> snackbarQueue = new LinkedList<>();
+    private Snackbar currentSnackbar;
+    private boolean isShowing = false;
+
     @SuppressLint("RestrictedApi")
     public void make_snackbar(Context context, String message) {
+        snackbarQueue.offer(message);
+        if(!isShowing){
+            showNextSnackbar(context);
+        }
+    }
+
+    private void showNextSnackbar(Context context) {
+        if (snackbarQueue.isEmpty()) {
+            isShowing = false;
+            return;
+        }
+
+        String message = snackbarQueue.poll();
 
         if (context instanceof Activity) {
             Activity activity = (Activity) context;
             View view = activity.findViewById(android.R.id.content);
 
             if (view != null) {
-                Snackbar snackbar = Snackbar.make(view, message, Snackbar.LENGTH_INDEFINITE);
+                isShowing = true;
+                currentSnackbar = Snackbar.make(view, message, Snackbar.LENGTH_INDEFINITE);
 
-                snackbar.setAction("Dismiss", v -> {});
+                currentSnackbar.setAction("Dismiss", v -> {
+                    currentSnackbar.dismiss();
+                    showNextSnackbar(context);
+                });
 
-                snackbar.show(); // Show the Snackbar after setting the action
+                currentSnackbar.addCallback(new Snackbar.Callback(){
+                    @Override
+                    public void onDismissed(Snackbar transientBottomBar, int event) {
+                        super.onDismissed(transientBottomBar, event);
+                        showNextSnackbar(context);
+                    }
+                });
 
+                currentSnackbar.show();
             } else {
                 Log.e("Snackbar", "Root view not found!");
                 Toast.makeText(context, "Error displaying message.", Toast.LENGTH_SHORT).show();
